0d7cee284b4777b89053f6f3d6ce87f3f892b4ca
0xsh4dy <rakshitawasthi17@gmail.com>
2023-05-24 10:29:32 +0530

    Initial commit

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/InfoSecIITR/kSentinel
+
+go 1.20
---
+++ core/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+void run(){
+
+}
+
+int main(){
+    std::cout<<"Starting kSentinel"<<std::endl;
+    while(1){
+        run();
+    }
+}
