93b125de44209f0ef414416c1b8542b7cdc6f58b
0xn4utilus <gyanendrabanjare8@gmail.com>
2023-06-09 22:37:48 +0530

    Add register and login

--- backend/auth/controllers/auth.go
+++ backend/auth/controllers/auth.go
@@ -1,15 +1,122 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"github.com/InfoSecIITR/kSentinel/auth/config"
+	"github.com/InfoSecIITR/kSentinel/auth/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
-func Register(c* fiber.Ctx)error{
+
+
+type UserParser struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type JWTClaims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func HashPassword(password string) string {
+	passwordByte := []byte(password)
+	hash, err := bcrypt.GenerateFromPassword(passwordByte, bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(hash)
+}
+
+func ComparePassword(hashPassword string, password string) error {
+	passwordByte := []byte(password)
+	hash := []byte(hashPassword)
+	err := bcrypt.CompareHashAndPassword(hash, passwordByte)
+	return err
+}
+
+func Register(c *fiber.Ctx) error {
+	userParser := new(UserParser)
+	if err := c.BodyParser(userParser); err != nil {
+		log.Println(err)
+	}
+	if userParser.Username == "" || userParser.Email == "" || userParser.Password == "" {
+		return c.Status(http.StatusBadRequest).SendString("All fields are required")
+	}
+	if len(userParser.Username) > 72 || len(userParser.Email) > 72 || len(userParser.Password) > 72 {
+		return c.Status(http.StatusBadRequest).SendString("Maximum length of feilds exceeded")
+	}
+
+	if user, err := models.GetUserInfoByUsername(userParser.Username); err == nil {
+		if user.Username != "" {
+			return c.Status(http.StatusBadRequest).SendString("Username already taken")
+		}
+	}
+	if user, err := models.GetUserInfoByEmail(userParser.Email); err == nil {
+		if user.Email != "" {
+			return c.Status(http.StatusBadRequest).SendString("Email already taken")
+		}
+	}
+	if err := models.InsertNewUser(userParser.Username, userParser.Email, HashPassword(userParser.Password)); err != nil {
+		return c.Status(http.StatusBadRequest).SendString("Registration failed")
+	}
+
 	return c.Status(http.StatusOK).SendString("Registration successful")
 }
 
-func Login(c* fiber.Ctx)error{
-	return c.Status(http.StatusOK).SendString("Login successful")
+func Login(c *fiber.Ctx) error {
+	userParser := new(UserParser)
+	if err := c.BodyParser(userParser); err != nil {
+		log.Println(err)
+	}
+
+	if userParser.Username == "" || userParser.Password == "" {
+		return c.Status(http.StatusBadRequest).SendString("All fields are required")
+	}
+
+	if len(userParser.Username) > 72 || len(userParser.Password) > 72 {
+		return c.Status(http.StatusBadRequest).SendString("Maximum length of feilds exceeded")
+	}
+
+	if userParser.Username != "" {
+		userDetails, _ := models.GetUserInfoByUsername(userParser.Username)
+		if userDetails.Username == "" {
+			return c.Status(http.StatusBadRequest).SendString("Username not found")
+		}
+		if err := ComparePassword(userDetails.Password, userParser.Password); err == nil {
+			expirationTime := time.Now().Add(7 * 24 * time.Hour)
+			claims := &JWTClaims{
+				Username: userDetails.Username,
+				RegisteredClaims: jwt.RegisteredClaims{
+					ExpiresAt: jwt.NewNumericDate(expirationTime),
+				},
+			}
+			token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+			// Create the JWT string
+			jwtKey  := []byte(config.JWT_SECRET)
+			tokenString, err := token.SignedString(jwtKey)
+			if err != nil {
+				log.Println(err)
+				return c.Status(http.StatusInternalServerError).SendString("Login Failed")
+			}
+			// Set Cookie
+			cookie := new(fiber.Cookie)
+			cookie.Name = "token"
+			cookie.Value = tokenString
+			cookie.Expires = expirationTime
+
+			c.Cookie(cookie)
+			return c.Status(http.StatusOK).SendString("Login successful")
+		}
+
+	}
+
+	return c.Status(http.StatusUnauthorized).SendString("Login Failed")
 }
--- backend/auth/main.go
+++ backend/auth/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"log"
 	"github.com/InfoSecIITR/kSentinel/auth/router"
+	"github.com/InfoSecIITR/kSentinel/auth/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	if err := models.InitDB(); err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
 	router.StartRouter(app)
 	app.Listen(":8080")
